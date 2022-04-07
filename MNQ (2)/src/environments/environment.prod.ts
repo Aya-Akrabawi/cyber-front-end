d6d5a2b1d48e6d17468443345876fab848115c10
Aya-Akrabawi <NunQWmj5mqDf5mSQsMJm1S5qGszWfUM9iVRaIOmlCxY>
2022-04-07 09:22:56 +0300

    delete compressed files

--- MNQ (2)/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  baseURL: 'https://cyber-project-master.herokuapp.com',
-  fileSize: 1048576,
-  fileAllowedExt: "['.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG', '.pdf']"
-};
