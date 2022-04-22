8f244643ce643356602847b73fb01bfc5f161424
Aya-Akrabawi <NunQWmj5mqDf5mSQsMJm1S5qGszWfUM9iVRaIOmlCxY>
2022-04-22 16:43:45 +0300

    locally

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   // baseURL: 'https://cyber-app-api.herokuapp.com',
   baseURL: 'http://localhost:8080',
-
   fileSize: 1048576,
   fileAllowedExt: "['.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG', '.pdf', '.docx', '.xls', '.xlsx']"
 };
